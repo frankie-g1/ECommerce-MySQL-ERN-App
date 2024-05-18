7ccfd085628452833b2f4fc8430c883dc769b9f3
Frank Guarino <fguarino71@gmail.com>
2024-05-18 11:46:21 -0400

    basic REST

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/* PUT users listing. */
+router.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+});
+
+
+/* DELETE a user in users listing. */
+router.delete('/user', (req, res) => {
+res.send('Got a DELETE request at /user')
+});
+
 module.exports = router;
