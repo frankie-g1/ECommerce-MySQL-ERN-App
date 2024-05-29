1204c7857a340726acddfebe22a7b08f106db7c1
Frank Guarino <fguarino71@gmail.com>
2024-05-29 19:48:09 -0400

    products listings

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Shop } from './pages/shop/shop';
 
 
 class App extends Component{
@@ -23,20 +25,11 @@ class App extends Component{
     render() {
         return (
             <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p className="App-intro">
-                    {this.state.apiResponse}
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Shop/>}/>
+                    </Routes>
+                </Router>
             </div>
           );
     }
---
+++ client/src/products.js
@@ -0,0 +1,15 @@
+/* REPLACE THIS later with data from the mysql call */
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage: ""
+    },
+    {
+        id:2,
+        productName: "Android Phone",
+        price: 998.0,
+        productImage: ""
+    }
+]
